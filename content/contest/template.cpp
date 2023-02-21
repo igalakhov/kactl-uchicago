0a85bef7231a09e31c52431207645537c5340ae8
Henry Cafaro <67771343+hjcafaroUC@users.noreply.github.com>
2023-02-20 21:06:13 -0600

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int r_d = 0;
+#ifdef DEBUG
+#define dbg(x) {++r_d; auto x_=x; --r_d; cerr<<string(r_d, '\t')<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+#else
+#define dbg(x)
+#endif
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v);
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v){
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
