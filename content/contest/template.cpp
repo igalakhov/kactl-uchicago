a5e386e4ff52ae583512a5f4e12519828d0e60ec
Henry Cafaro <67771343+hjcafaroUC@users.noreply.github.com>
2024-02-22 14:56:23 -0600

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int r_d = 0;
-#ifdef DEBUG
-#define dbg(x) {++r_d; auto x_=x; --r_d; cerr<<string(r_d, '\t')<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+template <typename T> ostream& operator+(ostream& os, const T& t);
 
+template<typename... Ts>
+ostream& operator<<(ostream& os,  const pair<Ts...>& p){ return os<<"{"<<p.first<<", "<<p.second<<"}"; }
+
+template<typename C>
+enable_if_t<!is_convertible_v<C, string>, ostream&> operator<<(ostream& os,  const C& v){
+    os<<"["; for(auto& x:v){os<<x<<", ";} return os<<"]";
+}
+
+template<typename... Ts>
+ostream& operator<<(ostream& os,  const tuple<Ts...>& tp){
+    os<<"{"; apply([&](auto&&... ts){(os + ... + ts);}, tp); return os << "}";
+}
+
+template <typename T>
+ostream& operator+(ostream& os, const T& t){ return (os << t << ", "); }
+
+template <typename... Ts>
+void dispatch(Ts&&... ts){ (std::cout + ... + ts);}
+
+#ifdef DEBUG
+#define dbg(x...) {cerr<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "; dispatch(x); cerr<<"\e[39m"<<endl;}
 #else
-#define dbg(x)
+#define dbg(x...)
 #endif
-template<typename Ostream, typename Cont>
-typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v);
-template<typename Ostream, typename ...Ts>
-Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
-	return os<<"{"<<p.first<<", "<<p.second<<"}";
-}
-template<typename Ostream, typename Cont>
-typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v){
-	os<<"[";
-	for(auto& x:v){os<<x<<", ";}
-	return os<<"]";
-}
 
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
